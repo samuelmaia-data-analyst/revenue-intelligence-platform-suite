dad785a8064c3a8215e87a41d771c025eff5a5e1
samuelmaiapro <smaia2@gmail.com>
2026-03-09 15:29:02 -0300

    fix: make dbt staging date casts compatible and update accepted_values syntax

--- modules/revenue-intelligence/dbt/models/staging/stg_customers.sql
+++ modules/revenue-intelligence/dbt/models/staging/stg_customers.sql
@@ -1,6 +1,6 @@
 select
     cast(customer_id as {{ dbt.type_int() }}) as customer_id,
-    cast(signup_date as {{ dbt.type_date() }}) as signup_date,
+    cast(signup_date as date) as signup_date,
     cast(channel as {{ dbt.type_string() }}) as channel,
     cast(segment as {{ dbt.type_string() }}) as segment
 from {{ source('raw', 'customers') }}
--- modules/revenue-intelligence/dbt/models/staging/stg_orders.sql
+++ modules/revenue-intelligence/dbt/models/staging/stg_orders.sql
@@ -1,6 +1,6 @@
 select
     cast(order_id as {{ dbt.type_string() }}) as order_id,
     cast(customer_id as {{ dbt.type_int() }}) as customer_id,
-    cast(order_date as {{ dbt.type_date() }}) as order_date,
+    cast(order_date as date) as order_date,
     cast(order_value as {{ dbt.type_numeric() }}) as order_value
 from {{ source('raw', 'orders') }}
